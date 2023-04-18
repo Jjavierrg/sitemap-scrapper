1ed071257f8c80f7bcf2696ed8c1b18d755a7794
Jose Javier Rodriguez Gallego <Jjavierrg@gmail.com>
2023-04-18 23:34:53 +0200

    Update sitemap-scrapper.ts

--- bin/sitemap-scrapper.ts
+++ bin/sitemap-scrapper.ts
@@ -4,7 +4,7 @@ import 'source-map-support/register';
 import { ssStack } from '../lib/stack';
 
 const app = new cdk.App();
-new sitempapStack(app, 'SitemapScrapperStack', {
+new ssStack(app, 'SitemapScrapperStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
